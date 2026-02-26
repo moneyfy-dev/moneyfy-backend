30adcb82bc4e4844bbaa27aa1716464dd8cbf79d
Alejandro Osses <alejandro.osses.r@gmail.com>
2026-02-26 18:28:02 -0300

    fix: anchor auth filters before authorization

--- src/main/java/com/referidos/app/segurosref/configs/SecurityConfig.java
+++ src/main/java/com/referidos/app/segurosref/configs/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -66,7 +67,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
             // Todo lo demás requiere autenticación
             .anyRequest().authenticated()
         )
-        .addFilterBefore(new DeviceValidationFilter(whiteListRepository, deviceRepository), JwtValidationFilter.class)
+        .addFilterBefore(new DeviceValidationFilter(whiteListRepository, deviceRepository), AuthorizationFilter.class)
         .addFilterAfter(jwtValidationFilter, DeviceValidationFilter.class)
         .build();
 }
