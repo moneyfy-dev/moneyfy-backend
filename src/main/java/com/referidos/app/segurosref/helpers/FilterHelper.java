6122853284fe4b50043d3d4c4106e92ec1130628
emartinezcode <eliu.martineez@gmail.com>
2026-06-09 22:52:06 -0400

    fix: bypass JWT validation for manager endpoints in SecurityConfig and FilterHelper

--- src/main/java/com/referidos/app/segurosref/helpers/FilterHelper.java
+++ src/main/java/com/referidos/app/segurosref/helpers/FilterHelper.java
@@ -16,7 +16,7 @@ public class FilterHelper {
         return endpoint.equals("/") || endpoint.equals("/swagger-ui.html") || endpoint.startsWith("/swagger-ui")
             || endpoint.startsWith("/v3/api-docs") || (endpoint.startsWith("/auth") && !endpoint.equals("/auth/disable/account"))
             || endpoint.startsWith("/seed" ) || endpoint.contains("/quoter/commission")
-            || endpoint.startsWith("/log") || endpoint.startsWith("/transaction");
+            || endpoint.startsWith("/log") || endpoint.startsWith("/transaction") || endpoint.startsWith("/api/v1/manager");
     }
 
 }
